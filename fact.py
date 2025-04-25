ba1eb7e2c1459b972a93969744a60c6039170ced
Pavand05 <23aiml092@bnmit.in>
2025-04-25 09:19:40 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,8 +1,8 @@
 def fact(n):
-    
-    # single line to find factorial
-    return 1 if (n==1 or n==0) else n * fact(n - 1) 
+    if(n==0orn==1)
+       return 1
+    else
+       return (n)*fact(n-1)
 
-# Driver Code
 num =input("Enter the number:")
 print(fact(num))
